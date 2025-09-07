580dc952180646f5475557956e0afa9e1e4991ea
tanaee <tanaaeeee@gmail.com>
2025-09-07 03:01:03 +0300

    redeploying

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,5 +2,5 @@ import css from './Loader.module.css'
 
 export default function Loader (){
     return(
-<p className={css.text}>Loading notes, please wait....</p>)
+<p className={css.text}>Loading notes, please wait...</p>)
 }
